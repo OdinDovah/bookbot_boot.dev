b1fae0ca04d2774efd06af44d78aeabb42596269
OdinDovah <pro@albertson-clan.com>
2025-08-10 21:51:04 -0400

    Fixed bug where word count didn't populate

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from stats import get_word_count
-from stats import get_char_count
-from stats import sorted_count
+from stats import *
 import sys
 
+
 def main():
-    if len(sys.argv) < 2:
-        print("Missing argument. Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
     file_path = sys.argv[1]
+    if len(sys.argv) < 2:
+        print("missing argument. usage: python3 main.py <path/to/book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}...")
     print("----------- Word Count ----------")
     print(f"Found {get_word_count(file_path)} total words")
     print("--------- Character Count -------")
     for count in sorted_count(get_char_count(file_path)):
-        print(f"{count["char"]}: {count["num"]}")
+        print(f"{count['char']}: {count['num']}")
     print("============= END ===============")
+
 
 main()
